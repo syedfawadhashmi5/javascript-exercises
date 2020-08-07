00e938ddf5f6c96ad605cfe4a0ae26930e822872
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-08-07 01:25:02 -0700

    Alert
    Alert examples

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("Error! please enter a vaild password");
+
+// alert("welcome to js Land... \n Happy Coding!");
+
+alert("welcome to js Land...");
+
+alert("Happy Coding!");
+
+
+alert("Hello... i can run JS");
