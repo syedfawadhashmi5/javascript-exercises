81f854139802d054ffbae60ee51f6baf1eb90953
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-08-07 02:25:39 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,92 @@
+
+                          //  alert
+
 // alert("Error! please enter a vaild password");
 
 // alert("welcome to js Land... \n Happy Coding!");
 
-alert("welcome to js Land...");
+// alert("welcome to js Land...");
 
-alert("Happy Coding!");
+// alert("Happy Coding!");
 
 
-alert("Hello... i can run JS");
+// alert("Hello... i can run JS");
+
+
+
+                        // variables for String
+
+// var UserName; // declared variable
+
+// var myName = "syed fawad hashmi"; // declared & assign variable
+
+
+// var massage = "hello world";
+
+
+// alert(massage);
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+
+// alert("my email is Address example@example.com");
+
+// var Book = "A smarter way \n to learn JavaScript"
+
+// alert(Book);
+
+// document.write("yah! i can write HTML.Content through JavaScript");
+
+
+// var abc = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert(abc);
+
+
+
+                        // variables for NUmber
+
+
+
+// var age = "i am 18 years old";
+
+// alert(age);
+
+
+// var VisitTime = "you have visited this site 23 times";
+
+
+// alert(VisitTime);
+
+
+// document.write("my birth year is 1996");
+
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store");
+
+
+
+
+                        // VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+// var name = "John Doe", userName = "user58", age = 23;
+
+
+// 5 legal  variable names.
+
+// var name;
+// var fullName;
+// var _9w;
+// var _userName
+// var Name;
+
+// 5 illegal variable names
+
+// Var var;
+// var 1j;
+// var 123;
+// var )();
+// var +ac
+
+document.write("Variable names can only contain, number , $ ,and _ , $my_variAbles");
+
