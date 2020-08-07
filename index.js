ed51b23cfb2fbf0bfa5fe83f1641cc50b6a70bb8
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-08-07 01:47:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+                          //  alert
+
+// alert("Error! please enter a vaild password");
+
+// alert("welcome to js Land... \n Happy Coding!");
+
+// alert("welcome to js Land...");
+
+// alert("Happy Coding!");
+
+
+// alert("Hello... i can run JS");
+
+
+
+                        // variables for String
+
+// var UserName; // declared variable
+
+// var myName = "syed fawad hashmi"; // declared & assign variable
+
+
+// var massage = "hello world";
+
+
+// alert(massage);
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+
+// alert("my email is Address example@example.com");
+
+// var Book = "A smarter way \n to learn JavaScript"
+
+// alert(Book);
+
+// document.write("yah! i can write HTML.Content through JavaScript");
+
+
+var abc = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(abc);
